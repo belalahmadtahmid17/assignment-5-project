d50e9f3ab881b4d3dc43581c710bd2ae074311a8
Belal Ahmad Tahmid <belalahmadtahmid17@gmail.com>
2026-04-11 19:48:34 +0600

    added the functionalities in donate and history buttons and updated popup section

---
+++ js/features.js
@@ -0,0 +1,31 @@
+sectionHandler('donation-btn','donation-section');
+
+sectionHandler('history-btn','history-section');
+
+
+function sectionHandler(btnID, sectionID){
+const btn =  document.getElementById(btnID);
+const section = document.getElementById(sectionID);
+
+btn.addEventListener('click',function(event){
+    event.preventDefault(); 
+
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
+    section.classList.remove('hidden');
+
+    document.getElementById('donation-btn').classList.add('border-gray-300')
+    document.getElementById('donation-btn').classList.add('border-[2px]')
+    document.getElementById('donation-btn').classList.remove('bg-lime-300')
+    
+    document.getElementById('history-btn').classList.add('border-gray-300')
+    document.getElementById('history-btn').classList.add('border-[2px]')
+    document.getElementById('history-btn').classList.remove('bg-lime-300')
+
+    btn.classList.add('bg-lime-300')
+    btn.classList.remove('border-gray-300');
+    btn.classList.remove('border-[2px]');
+
+})
+}
