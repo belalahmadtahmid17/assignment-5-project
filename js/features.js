717b82c151123d6328431c63d2d3c564633d6499
Belal Ahmad Tahmid <belalahmadtahmid17@gmail.com>
2026-04-20 00:49:20 +0600

    minor changes

--- js/features.js
+++ js/features.js
@@ -1,6 +1,6 @@
 document.getElementById('blog-btn').addEventListener('click',function(event){
     event.preventDefault();
-    window.location.href = "blog.html"
+    window.location.href = "./blog.html"
 })
 
 
